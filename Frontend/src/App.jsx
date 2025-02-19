3495f3eeb20fbe650f0c7ac617eb24e969500e61
Priyanshu Rajan <priyanshumth0808@gmail.com>
2025-02-19 14:22:45 +0530

    add: Frontend Setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <main>
+        <div className="left">
+          <div className="code"></div>
+          <div className="review"></div>
+        </div>
+        <div className="right"></div>
+      </main>
+    </>
+  );
+}
+
+export default App;
